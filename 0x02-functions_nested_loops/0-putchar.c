c8eea8a60c4cb74f5a7bababc2f73b4fb086e90c
shehab mohamed <shehabbendary0@gmail.com>
2023-07-18 05:10:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,10 @@
  *
  */
 
-#include "holberton.h"
+#include "_putchar"
 
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
